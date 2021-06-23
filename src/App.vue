4175b8e84c7460e71647447f325df2c5c2516ffa
HiroyukiChiba <hiroyuki.chiba3028@gmail.com>
2021-06-23 15:41:36 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Todo :todos="state.todos" @add-todo="addTodo" />
+</template>
+
+<script>
+import {reactive} from 'vue';
+import Todo from './components/Todo.vue'
+
+export default {
+  components: {
+    Todo
+  },
+  setup () {
+    const state = reactive({
+      todos: ['ネギ']
+    });
+    const addTodo = (todo) => {
+      state.todos.push(todo)
+    }
+    return {
+      state,
+      addTodo
+    }
+  }
+}
+</script>
---
+++ src/components/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input v-model="state.newTodo" type="text">
+    <button type="button" @click="addTodo">追加</button>
+  </div>
+  <ul>
+    <li v-for="todo in todos">
+      <input type="checkbox">
+      <span>{{ todo }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+import {reactive} from 'vue'
+
+export default {
+  props: {
+    todos: {required: true, type: Array}
+  },
+  setup(props, context) {
+    const state = reactive({
+      newTodo: null
+    });
+    const addTodo = () => {
+      console.log(state.newTodo)
+      context.emit('add-todo', state.newTodo);
+    };
+    return {
+      state,
+      addTodo,
+    }
+  }
+}
+</script>
